62f00b5f75965ff0c8b4beb0e338cb06205b7a81
JuliooS89 <123308373+JuliooS89@users.noreply.github.com>
2025-09-29 21:35:53 -0300

    feat: agregar autenticación JWT con AuthService, AuthController y protección de endpoints

---
+++ Models/Usuario.cs
@@ -0,0 +1,6 @@
+public class Usuario
+{
+    public string NombreUsuario { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Rol { get; set; } = string.Empty;
+}
